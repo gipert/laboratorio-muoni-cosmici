827ecb779f60d4095eede41a472368d493e06336
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2016-11-19 22:38:59 +0100

    Argomenti command line invece che runtime, Makefile
    Faggio ho fatto un makefile per me e te, basta che lanci "make" nella
    cartella dove c'è il Makefile

--- code/montecarlo/montecarlo.cc
+++ code/montecarlo/montecarlo.cc
@@ -1,7 +1,17 @@
+// MonteCarlo per la misura della vita media dei muoni cosmici in alluminio
+//
+// Authors: Mattia Faggin, Davide Piras, Luigi Pertoldi
+//
+// Prova:
+// ./montecarlo --help
+//
+
 // nel codice utilizziamo 3 metodi per stimare la baseline: 1-media, 2-fit likelihood, 3-fit chi2
 
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <string>
 #include "TApplication.h"
 #include "TH1.h"
 #include "TCanvas.h"
@@ -14,19 +24,38 @@
 #define StartBase 2138	// punto dell'istogramma in cui comincia la baseline
 #define End 3904	// fine istogramma
 
-int main(int argc, char* argv[]) {
+int main( int argc, char* argv[] ) {
 
+    // salvo gli argomenti prima di darli in pasto al TApplication
+    // perchè lui li modifica
+    std::vector<std::string> args;
+    args.reserve(argc);
+    for ( int i = 0; i < argc; i++ ) args[i] = argv[i];
+
+    if ( argc == 2 and args[1] == "--help" ) {
+        std::cout << std::endl
+                  << "MonteCarlo per la misura della vita media dei muoni cosmici in alluminio." << std::endl
+                  << "Autori: Mattia Faggin, Davide Piras, Luigi Pertoldi" << std::endl << std::endl
+                  << "Utilizzo:" << std::endl 
+                  << "    $ ./montecarlo [numeroConteggi] [semeGeneratore] [rebinFactor]" << std::endl << std::endl;
+        return 0;
+    }
+
+    if ( argc < 4 ) {
+        std::cout << "Pochi argomenti! Se non ti ricordi c'è l'opzione '--help'" << std::endl
+                  << "Termino l'esecuzione..." << std::endl;
+        return 0;
+    }
+
+    int counts    = std::stoi(args[1]);
+    int seed      = std::stoi(args[2]);
+    int RebFactor = std::stoi(args[3]);
+    
     TApplication Root("App",&argc,argv);   
     TCanvas can("can","Simulazione MC",800,500);
 
     // simulazione della baseline
     TH1F baseline("baseline","baseline",4096,0,4096);
-    int counts;
-    std::cout<<"\nConteggi = ";
-    std::cin>>counts;
-    int seed;
-    std::cout<<"\nSeme = ";
-    std::cin>>seed;		// inserendo da tastiera semi diversi non abbiamo sempre lo stesso istogramma
     TRandom3 r;
     r.SetSeed(seed);
     int k=0;
@@ -35,14 +64,11 @@ int main(int argc, char* argv[]) {
     {
         c=r.Uniform(Begin,End);
         baseline.Fill(c);
-	if (c>StartBase) k++;	
-	// 'counts' è l'integrale della baseline solo nella parte finale dello spettro
-	// il contatore k è incrementato solo se il numero generato è oltre StartBase
+	    if (c>StartBase) k++;	
+	    // 'counts' è l'integrale della baseline solo nella parte finale dello spettro
+	    // il contatore k è incrementato solo se il numero generato è oltre StartBase
     }
     // rebin dell'istogramma
-    int RebFactor;
-    std::cout<<"\nRebin = ",
-    std::cin>>RebFactor;
     baseline.Rebin(RebFactor); 
 
     // 1 - metodo della MEDIA
@@ -50,22 +76,24 @@ int main(int argc, char* argv[]) {
     int N = 0;
     float Mean = 0;
     float ErrMean = 0;
+    // calcolo della media
     for(int j=1; j<=(baseline.GetNbinsX()); j++)
     {
         float BinCenter = baseline.GetBinCenter(j);
-	if( BinCenter>StartBase ) 
-	{
-		N++;
-		Mean += baseline.GetBinContent(j);
-	}
+	    if( BinCenter>StartBase ) 
+	    {
+		    N++;
+		    Mean += baseline.GetBinContent(j);
+	    }
     }
     Mean = Mean/N;
+    // calcolo l'errore della media
     for(int i=1; i<=(baseline.GetNbinsX()); i++)
     {
-	if( (baseline.GetBinCenter(i))>StartBase ) 
-	{
-		ErrMean += pow(baseline.GetBinContent(i)-Mean,2);
-	}
+	    if( (baseline.GetBinCenter(i))>StartBase ) 
+	    {
+		    ErrMean += pow(baseline.GetBinContent(i)-Mean,2);
+	    }
     }
     ErrMean =ErrMean/(N-1);
     ErrMean =sqrt(ErrMean/N); // la singola misura come errore ha lo scarto quadratico medio
@@ -75,13 +103,14 @@ int main(int argc, char* argv[]) {
     // 3 - metodo del FIT CHI2
     TFitResultPtr BaseChi2Ptr = baseline.Fit("pol0","S","",StartBase,End);
 
-    std::cout<<"\n***RISULTATI***";
-    std::cout<<"\nMedia baseline = "<< Mean << " +- "<< ErrMean;
-    std::cout<<"\nFit Likelihood baseline = "<< BaseLikePtr->Parameter(0) << " +- "<< BaseLikePtr->ParError(0);
-    std::cout<<"\nFit Chi2 baseline = "<< BaseChi2Ptr->Parameter(0) << " +- "<< BaseChi2Ptr->ParError(0)<<std::endl;
+    std::cout <<"\n***RISULTATI***"
+              <<"\nMedia baseline = "          << Mean                      << " +- " << ErrMean
+              <<"\nFit Likelihood baseline = " << BaseLikePtr->Parameter(0) << " +- " << BaseLikePtr->ParError(0)
+              <<"\nFit Chi2 baseline = "       << BaseChi2Ptr->Parameter(0) << " +- " << BaseChi2Ptr->ParError(0) << std::endl;
 
     baseline.Draw();
     Root.Run();
+
     return 0;
 }
 
